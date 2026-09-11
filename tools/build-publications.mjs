fc6b0868851cff346154b7c5f36248e56ef63db1
AleaScript <aleascript@proton.me>
2026-09-11 22:51:53 +0200

    Keep custom covers outside Vivliostyle cover entries

--- tools/build-publications.mjs
+++ tools/build-publications.mjs
@@ -496,7 +496,7 @@ function publicationCoverMarkdown(
 
   return `<div class="publication-cover">
   <div class="publication-cover__visual">
-    <img role="doc-cover" class="publication-cover__image" src="${escapeHtml(cover.image)}" alt="${escapeHtml(cover.alt ?? localeConfig.title)}" />
+    <img class="publication-cover__image" src="${escapeHtml(cover.image)}" alt="${escapeHtml(cover.alt ?? localeConfig.title)}" />
   </div>
   <div class="publication-cover__text">
     ${seriesTitle}
@@ -660,7 +660,7 @@ async function preparePublication(
     language: locale,
     size: publication.size ?? 'A4',
     entry: [
-      ...(coverEntry ? [{path: coverEntry, rel: 'cover'}] : []),
+      ...(coverEntry ? [coverEntry] : []),
       {rel: 'contents'},
       ...entries,
     ],
@@ -671,14 +671,6 @@ async function preparePublication(
       title: localeConfig.tocTitle ?? (locale === 'fr' ? 'Sommaire' : 'Contents'),
       sectionDepth: localeConfig.toc?.sectionDepth ?? 2,
     },
-    ...(localeConfig.cover
-      ? {
-          cover: {
-            src: localeConfig.cover.image,
-            name: localeConfig.cover.alt ?? localeConfig.title,
-          },
-        }
-      : {}),
     output,
     workspaceDir: '.vivliostyle',
     ...(hasStaticImages ? {static: {'/img': staticImageDestination}} : {}),
