7ae5cc9694775da03f0f1fa1a06f9a378525beec
aleascript <aleascript@gmail.com>
2026-09-12 23:17:04 +0200

    remove isFrench from sidebar

--- sidebars.ts
+++ sidebars.ts
@@ -179,37 +179,35 @@ const sidebars: SidebarsConfig = {
           ],
         },
         'narratives/gurdtars-banishment/index',
-        isFrench
-          ? {
+        {
+          type: 'category',
+          label: 'La Route de Talimar',
+          link: {type: 'doc', id: 'narratives/talimar-road/index'},
+          items: [
+            {
               type: 'category',
-              label: 'La Route de Talimar',
-              link: {type: 'doc', id: 'narratives/talimar-road/index'},
+              label: 'Session 0',
+              link: {type: 'doc', id: 'narratives/talimar-road/session-0/index'},
               items: [
-                {
-                  type: 'category',
-                  label: 'Session 0',
-                  link: {type: 'doc', id: 'narratives/talimar-road/session-0/index'},
-                  items: [
-                    'narratives/talimar-road/session-0/verbatim',
-                    'narratives/talimar-road/session-0/narrative',
-                    'narratives/talimar-road/session-0/backstage',
-                    'narratives/talimar-road/session-0/portrait',
-                  ],
-                },
-                {
-                  type: 'category',
-                  label: 'Session 1',
-                  link: {type: 'doc', id: 'narratives/talimar-road/session-1/index'},
-                  items: [
-                    'narratives/talimar-road/session-1/verbatim',
-                    'narratives/talimar-road/session-1/narrative',
-                    'narratives/talimar-road/session-1/backstage',
-                    'narratives/talimar-road/session-1/portrait',
-                  ],
-                },
+                'narratives/talimar-road/session-0/verbatim',
+                'narratives/talimar-road/session-0/narrative',
+                'narratives/talimar-road/session-0/backstage',
+                'narratives/talimar-road/session-0/portrait',
               ],
-            }
-          : 'narratives/talimar-road/index',
+            },
+            {
+              type: 'category',
+              label: 'Session 1',
+              link: {type: 'doc', id: 'narratives/talimar-road/session-1/index'},
+              items: [
+                'narratives/talimar-road/session-1/verbatim',
+                'narratives/talimar-road/session-1/narrative',
+                'narratives/talimar-road/session-1/backstage',
+                'narratives/talimar-road/session-1/portrait',
+              ],
+            },
+          ],
+        }
       ],
     },
     {
