467258183704ec62b64a2a34fa36a6e33d62066b
aleascript <aleascript@gmail.com>
2026-09-19 10:04:21 +0200

    change calver

--- publications.config.mjs
+++ publications.config.mjs
@@ -175,7 +175,7 @@ export default definePublications({
   publications: {
     guide: {
       author: 'AleaScript',
-      version: '2026-09-17',
+      version: '2026-09-19',
       status: 'Draft',
       lineage: {
         designedWith: {
